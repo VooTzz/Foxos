367b838296126c40bfdf951f25e054b75304ea3a
VooTzz <jsvioskevkk@gmail.com>
2025-08-05 21:23:22 +0700

    pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // src/pages/_app.tsx
 
 import type { AppProps } from 'next/app';
-import '../globals.css';
+import '../styles/globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
